3196c351e3acf476ec488f6d8c6e8264bbacac72
pcmarks <peter.c.marks@gmail.com>
2016-08-18 09:46:36 -0400

    Initial Commit

---
+++ lib/genstage_example_3.exs
@@ -0,0 +1,60 @@
+alias Experimental.GenStage
+
+defmodule GenStageExample3 do
+
+  defmodule Split do
+    @moduledoc """
+    This GenStage example is meant to illustrate the splitting of an output flow of
+    events - a list of integers - to more than one stage. This is accomplished by
+    using the PartitionDispatcher dispatcher and a "splitter" function that shunts
+    even integers to one partition and odd integers to another partition. For a more
+    detailed explanation, please see this blog [post](www.elixirfbp.com)
+    """
+    use GenStage
+
+    def init(_) do
+      {:producer_consumer, %{},
+        dispatcher: {GenStage.PartitionDispatcher,
+                      partitions: 2,
+                      hash: &splitter/2}}
+    end
+
+    @doc """
+    The "hash function"
+    """
+    def splitter(event, no_of_partitions ) do
+      {event, rem(event, no_of_partitions)}
+    end
+
+    @doc """
+    Simply pass input events onto the partition dispatcher
+    """
+    def handle_events(events, _from, state) do
+      {:noreply, events, state}
+    end
+  end
+
+  defmodule Ticker do
+    use GenStage
+    def init(state) do
+      {:consumer, state}
+    end
+    def handle_events(events, _from, {sleeping_time, name} = state) do
+      IO.puts "Ticker(#{name}) events: #{inspect events, charlists: :as_lists}"
+      Process.sleep(sleeping_time)
+      {:noreply, [], state}
+    end
+  end
+
+  {:ok, inport} = GenStage.from_enumerable(1..10)
+  {:ok, split}  = GenStage.start_link(Split, 0)
+  {:ok, evens}  = GenStage.start_link(Ticker, {2_000, :evens})
+  {:ok, odds}   = GenStage.start_link(Ticker, {2_000, :odds})
+
+  GenStage.sync_subscribe(evens, to: split, partition: 0, max_demand: 1)
+  GenStage.sync_subscribe(odds,  to: split, partition: 1, max_demand: 1)
+  GenStage.sync_subscribe(split, to: inport, max_demand: 1)
+
+  Process.sleep(:infinity)
+
+end
---
+++ test/genstage_example_3_test.exs
@@ -0,0 +1,8 @@
+defmodule GenstageExample3Test do
+  use ExUnit.Case
+  doctest GenstageExample3
+
+  test "the truth" do
+    assert 1 + 1 == 2
+  end
+end
